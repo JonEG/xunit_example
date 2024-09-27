41f67c88f08d81df5a0e29da25d875103eab7775
Jon Echeveste González <j.echeveste-gonzales@ovan.de>
2024-09-27 11:08:00 +0200

    first commit

---
+++ bin/xunit_example.dart
@@ -0,0 +1,5 @@
+import 'package:xunit_example/xunit_example.dart' as xunit_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${xunit_example.calculate()}!');
+}
---
+++ test/xunit_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:xunit_example/xunit_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
